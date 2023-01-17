e68c1f5da2a14aaf7aab0016414ae9a9a1f4b9c4
brezzy1337 <90067281+brezzy1337@users.noreply.github.com>
2023-01-16 20:52:37 -0600

    Dockerfile dev update

--- src/utils/trpc.utils/OpenAPI.ts
+++ src/utils/trpc.utils/OpenAPI.ts
@@ -4,10 +4,10 @@ import trcpRouter from '@src/routes/trpcRouter';
 
 // Generate OpenAPI schema document
 export const openApiDocument = generateOpenApiDocument(trcpRouter, {
-  title: 'Example CRUD API',
+  title: 'Homegrown CRUD API',
   description: 'OpenAPI compliant REST API built using tRPC with Express',
   version: '1.0.0',
-  baseUrl: 'http://localhost:3001/api',
+  baseUrl: 'http://localhost:1337/api',
   docsUrl: 'https://github.com/jlalmes/trpc-openapi',
   tags: ['auth', 'users', 'posts'],
 });
